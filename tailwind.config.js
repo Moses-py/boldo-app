2e6fcf887921244c5a66372ad6835523e4e6911b
Moses Chukwunekwu <dantereus1@gmail.com>
2023-03-14 12:19:31 +0100

    Added tailwind configs and custom styling

---
+++ tailwind.config.js
@@ -0,0 +1,55 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx}",
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+
+    // Or if using `src` directory:
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    colors: {
+      primary: "#0A2640",
+      hero: "#65E4A3",
+      textSlate: "#F1F1F1",
+      textGrey: "#777777",
+      textLight: "#FFFFFF",
+      textDark: "#000000",
+      textGreen: "#69E6A6",
+    },
+    screens: {
+      sm: "480px",
+      md: "768px",
+      lg: "976px",
+      xl: "1440px",
+    },
+    fontSize: {
+      xs: "16px",
+      sm: "20px",
+      md: "24px",
+      lg: "28px",
+      xl: "48px",
+      "2xl": "96px",
+    },
+    fontFamily: {
+      primary: ["Manrope", "sans - serif"],
+      secondary: ["Open Sans", "sans - serif"],
+    },
+    lineHeight: {
+      24: "24px",
+      28: "28px",
+      32: "32px",
+      36: "36px",
+      48: "48px",
+      56: "56px",
+    },
+    fontWeight: {
+      normal: "400",
+      semibold: "600",
+      bold: "700",
+    },
+    extend: {},
+  },
+  plugins: [],
+};
